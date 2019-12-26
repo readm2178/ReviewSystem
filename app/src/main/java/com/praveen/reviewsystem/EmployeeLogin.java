ba01ae378f28cbe7c5ac4b0a056385bc32971ac7
Praveen <readm2178@gmail.com>
2019-12-26 14:55:52 +0530

    Intent initial

---
+++ app/src/main/java/com/praveen/reviewsystem/EmployeeLogin.java
@@ -0,0 +1,4 @@
+package com.praveen.reviewsystem;
+
+public class EmployeeLogin {
+}
--- app/src/main/java/com/praveen/reviewsystem/MainActivity.java
+++ app/src/main/java/com/praveen/reviewsystem/MainActivity.java
@@ -1,12 +1,18 @@
 package com.praveen.reviewsystem;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 import android.view.Window;
 import android.view.WindowManager;
+import android.widget.Button;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
-
+    
+    TextView Account,CompanyText,EmployeeTxtView;
+    Button LoginButton1,LoginButton2;
 
 
     @Override
@@ -17,9 +23,37 @@ public class MainActivity extends AppCompatActivity {
 //        this.getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,
 //                WindowManager.LayoutParams.FLAG_FULLSCREEN); //enable full screen
         setContentView(R.layout.activity_main);
-//sangeetha initial commit
 
-        //hello
+        initialize();
+        onClick();
 
+
+    }
+
+    private void initialize() {
+        Account =(TextView)findViewById(R.id.Account);
+        CompanyText =(TextView)findViewById(R.id.CompanyText);
+        EmployeeTxtView =(TextView)findViewById(R.id.EmployeeTxtView);
+        LoginButton1 =(Button) findViewById(R.id.LoginButton1);
+        LoginButton2 =(Button) findViewById(R.id.loginbutton2);
+
+    }
+
+    private void onClick() {
+
+        LoginButton1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent company = new Intent(MainActivity.this,Login.class);
+                startActivity(company);
+            }
+        });
+        LoginButton2.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent Employee = new Intent(MainActivity.this,EmployeeLogin.class);
+                startActivity(Employee);
+            }
+        });
     }
 }
