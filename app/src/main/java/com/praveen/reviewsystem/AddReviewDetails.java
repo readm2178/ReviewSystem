77eacb87e6b09e11a9f47efb0c2a74eaef037907
Praveen <readm2178@gmail.com>
2019-12-26 12:53:14 +0530

    Add Review deatails

--- app/src/main/java/com/praveen/reviewsystem/AddReviewDetails.java
+++ app/src/main/java/com/praveen/reviewsystem/AddReviewDetails.java
@@ -1,4 +1,20 @@
 package com.praveen.reviewsystem;
 
-public class AddReviewDetails {
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+
+public class AddReviewDetails extends AppCompatActivity {
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.add_review_details);
+
+
+    }
+
 }
