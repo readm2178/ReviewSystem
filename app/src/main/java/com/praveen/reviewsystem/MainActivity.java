36c91dcae0140bb890cada1647127b53478e6213
SangeeMega <sangeethameganathan16@gmail.com>
2020-01-09 15:18:46 +0530

    company review list

--- app/src/main/java/com/praveen/reviewsystem/MainActivity.java
+++ app/src/main/java/com/praveen/reviewsystem/MainActivity.java
@@ -10,34 +10,34 @@ import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
 
-   // Button CompanyLogin,EmployeeLogin;
+    Button CompanyLogin,EmployeeLogin;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-//        initialize();
-//        onClick();
-//    }
-//
-//
-//
-//    private void initialize() {
-//
-//        CompanyLogin = (Button)findViewById(R.id.CompanyLoginButton);
-//        EmployeeLogin = (Button)findViewById(R.id.EmployeeLoginButton);
-//    }
-//
-//    private void onClick() {
-//
-//        CompanyLogin.setOnClickListener(new View.OnClickListener() {
-//            @Override
-//            public void onClick(View view) {
-//
-//                Intent Company = new Intent(MainActivity.this,Login.class);
-//                startActivity(Company);
-//            }
-//        });
+        initialize();
+        onClick();
+    }
+
+
+
+    private void initialize() {
+
+        CompanyLogin = (Button)findViewById(R.id.CompanyLoginButton);
+        EmployeeLogin = (Button)findViewById(R.id.EmployeeLoginButton);
+    }
+
+    private void onClick() {
+
+        CompanyLogin.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+                Intent Company = new Intent(MainActivity.this,Login.class);
+                startActivity(Company);
+            }
+        });
     }
 }
